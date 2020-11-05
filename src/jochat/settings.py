c2764bef505636a6b1b4f0b038594185a65abb61
Joshuacodeat10 <joshuaagboola99@gmail.com>
2020-11-05 14:47:40 +0100

    another commit

--- src/jochat/settings.py
+++ src/jochat/settings.py
@@ -153,4 +153,4 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn","static_root"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn","media_root")
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
