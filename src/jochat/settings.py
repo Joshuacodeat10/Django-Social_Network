f05adf3e90e3901a07b40260a80e1af87494dad3
Joshuacodeat10 <joshuaagboola99@gmail.com>
2020-11-05 15:02:16 +0100

    another

--- src/jochat/settings.py
+++ src/jochat/settings.py
@@ -141,7 +141,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static and files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
